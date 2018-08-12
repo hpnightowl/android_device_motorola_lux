7851aa80fe19cd9c94d2b4033636215741a73fe2
Kyle Harrison <khwebmail@gmail.com>
2018-08-12 19:02:21 +0100

    [TEMP] lux: Disable SELinux for 12 bringup
    - Set kernel to permissive mode
    - Disable the incudes of sepolicy files
    - Remove context on /fsg & /firmware
    
    [TEMP] msm8916-common: Add temporary sepolicies for 12.0 bringup
    
    [TEMP] msm8916-common: Allow neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,6 +59,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := \
 # Kernel
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M androidboot.bootdevice=7824900.sdhci utags.blkdev=/dev/block/bootdevice/by-name/utags utags.backup=/dev/block/bootdevice/by-name/utagsBackup movablecore=160M
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
@@ -158,9 +159,12 @@ TARGET_USES_OLD_MNC_FORMAT := true
 BOARD_ROOT_EXTRA_FOLDERS := firmware persist fsg
 
 # SELinux
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
-include device/qcom/sepolicy-legacy/sepolicy.mk
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+#include device/qcom/sepolicy-legacy/sepolicy.mk
+#BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy_tmp
+
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Sensors
 BOARD_USES_STML0XX_SENSOR_HUB := true

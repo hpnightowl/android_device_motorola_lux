b015de901788e17e84298883e73cd51aa222b01b
Kyle Harrison <khwebmail@gmail.com>
2021-12-09 20:23:04 +0000

    lux: Revert "msm8916-common: Create system copy of ims-ext-common and add to classpath"
    This reverts commit 5cf5f312f4a32285f12040765670d5dc492fe0a0.
    
    We now have a modified ims.apk which loads ims-ext-common so this can be reverted.

--- device.mk
+++ device.mk
@@ -194,16 +194,13 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    ims-ext-common_system \
+    ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
     libshims_ims
-
-PRODUCT_BOOT_JARS += \
-    ims-ext-common_system
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml

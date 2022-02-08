baacb8538b1d8eaed78c3b63d043da41ef9edfd9
hpnightowl <iamhp2k@gmail.com>
2022-02-09 05:13:48 +0530

    lux: dalvik :props: handled by go config

--- system_prop.mk
+++ system_prop.mk
@@ -4,15 +4,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.fluence.voicecall=true \
     ro.vendor.audio.sdk.fluencetype=fluence
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=192m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=2m \
-    dalvik.vm.heapmaxfree=8m
-
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.mot_ecc_enabled=1
